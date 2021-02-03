9da62abe821c63407492f20d954d789dfc066e0a
shubhamuwu <43602057+shubhamuwu@users.noreply.github.com>
2021-02-03 20:19:14 +0530

    Create 1stproject.py

---
+++ 1stproject.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('https://raw.githubusercontent.com/AdiPersonalWorks/Random/master/student_scores%20-%20student_scores.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print(x)
+print(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x , y, test_size = 0.2 , random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue' )
+plt.title('marks(training set)')
+plt.xlabel("hours study")
+plt.ylabel("salary")
+plt.show()
+
+plt.scatter(x_test, y_test, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue' )
+plt.title('marks(test set)')
+plt.xlabel("hours study")
+plt.ylabel("salary")
+plt.show()
+
+print(regressor.predict([[9.5]]))
